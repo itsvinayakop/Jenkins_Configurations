6674e6278aec07f709187e0c05aebbb4e1d7aa87
Vinayak Bansal <vinayak.banslsahab@gmail.com>
2025-01-20 17:40:26 +0530

    Full code

--- src/test/java/LoginAutomationTest.java
+++ src/test/java/LoginAutomationTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class LoginAutomationTest {
     public static void main(String[] args) {
@@ -48,14 +50,39 @@ public class LoginAutomationTest {
             signInButton.click();
 
             // Wait for the account link to appear
-            WebElement myAccountLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("account"))); // Adjust locator as needed
+            WebElement myAccountLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Automation']"))); // Adjust locator as needed
 
             // Verify login success
+
             if (myAccountLink.isDisplayed()) {
                 System.out.println("Login successful!");
             } else {
                 System.out.println("Login failed.");
             }
+
+            Thread.sleep(3);
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Subscription']")));
+            WebElement element= driver.findElement(By.xpath("//h2[text()='Subscription']"));
+            scrollToElement(element,driver);
+
+
+
+
+            wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='choose']//a")));
+            List<WebElement> addCartButton= driver.findElements(By.xpath("//div[@class='choose']//a"));
+            System.out.println("Total  Btns:"+ addCartButton.size());
+
+            WebElement addToCart = addCartButton.get(2);
+            addToCart.click();
+        
+            Thread.sleep(5000);
+
+            // LOG OUT FUNCTIONALITY
+//            WebElement logOut = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()=' Logout']")));
+//            logOut.click();
+//            System.out.println("User Logged Out");
+
 
         } catch (Exception e) {
             System.err.println("An error occurred during the test: " + e.getMessage());
@@ -66,5 +93,12 @@ public class LoginAutomationTest {
                 driver.quit();
             }
         }
+    }
+
+
+    public static void scrollToElement(WebElement element, WebDriver driver) throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
+        Thread.sleep(500);
     }
 }
